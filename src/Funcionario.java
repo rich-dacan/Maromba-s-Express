fa50edac5d34e441bc7dec4ba116f491ceaec663
Richard Cândido <rich_dacan@protonmail.com>
2024-04-06 20:27:34 -0300

    feat :sparkles: attributes and costructor to Funcionario class

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,3 +1,16 @@
 public class Funcionario {
+  private String rg;
+  private String nome;
+  private String Departamento;
+  private double salario;
+  private String ativo;
+
+  public Funcionario(String rg, String nome, String departamento, double salario, String ativo) {
+    this.rg = rg;
+    this.nome = nome;
+    Departamento = departamento;
+    this.salario = salario;
+    this.ativo = ativo;
+  }
 
 }
