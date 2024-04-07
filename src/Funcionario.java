0ebf4056cd8ef70a77ae216bf4e1b431711a0123
Léo Sartorelli <132115209+leosartorelli1@users.noreply.github.com>
2024-04-07 11:23:18 -0300

    feat: Loop no metodo main , getters e setters na classe funcionario
    Implementado na classe funcionario os getters e setters para a maioria das classes (ainda restando o atributo bonificar, salario, e isAtivo).
    Na classe GerenciarFuncionario implementado no fluxo principal do método main o loop para exibir um menu.

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,3 +1,59 @@
 public class Funcionario {
-    // estou aprendendo a usar o git 
+
+    private double salario;
+    private String nome;
+    private String departamento;
+    private int rg;
+    private boolean ativo;
+
+
+    public void atualizarSalario(double aumento){
+        salario += aumento;
+    }
+    public void exibirDados(){
+        System.out.println("RG = " + rg);
+        System.out.println("nome = " + nome);
+        System.out.println("departamento = " + departamento);
+    }
+
+
+    public int rg() {
+        return rg;
+    }
+
+    public void rg(int rg) {
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
 }
